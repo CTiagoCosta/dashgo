087f3ebd872b844a928fa3dcfba18347707ed84f
Carlos <tiagocdz29@hotmail.com>
2023-07-06 17:02:35 -0400

    Criação dos components, pages, dashboard, graficos e instalação de pacotes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    ) 
 }
